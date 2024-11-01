b7effdbb540ebc290d3fcd74e586f509b8840ea4
Amr khaled <Amr khaled@DESKTOP-551V1S8>
2024-11-01 20:24:49 +0200

    hello dev

--- DiabetesApp.API/Program.cs
+++ DiabetesApp.API/Program.cs
@@ -4,7 +4,7 @@ namespace DiabetesApp.API
 	{
 		public static void Main(string[] args)
 		{
-			// hello master
+			// hello Dev
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
