ec1d171a1690ffe99d568f3d5ab51cc792d27368
Amr khaled <Amr khaled@DESKTOP-551V1S8>
2024-11-01 20:25:47 +0200

    create step1

--- DiabetesApp.API/Program.cs
+++ DiabetesApp.API/Program.cs
@@ -4,7 +4,7 @@ namespace DiabetesApp.API
 	{
 		public static void Main(string[] args)
 		{
-			// hello Dev
+			// hello step 1
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
